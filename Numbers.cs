a8d2a02e5d9584b7a66d65679485e80cf8e2a1a0
adepra22 <ade.prasetyo@students.amikom.ac.id>
2020-04-07 08:18:44 +0700

    Membuat Class Numbers

---
+++ Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Method
+{
+    class Numbers
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+
+        }
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+
+        }
+        public int FindMaximum(int number1, int number2)
+        {
+
+        }
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+
+        }
+    }
+}
